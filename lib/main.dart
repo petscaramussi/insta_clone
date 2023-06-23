2a2cb0b126559e0029de770ea4b0659f0a8935ba
PetScaramussi <peterson.silva6@fatec.sp.gov.br>
2023-06-23 14:49:50 -0300

    User Data State Management

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:insta_clone/providers/user_provider.dart';
 import 'package:insta_clone/responsive/mobile_screen_layout.dart';
 import 'package:insta_clone/responsive/responsive_layout_screen.dart';
 import 'package:insta_clone/responsive/web_screen_layout.dart';
@@ -8,33 +9,31 @@ import 'package:insta_clone/screens/login_screen.dart';
 import 'package:insta_clone/screens/signup_screen.dart';
 import 'package:insta_clone/utils/colors.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 
-
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-    if(kIsWeb) {
-      await Firebase.initializeApp(
+  if (kIsWeb) {
+    await Firebase.initializeApp(
         options: const FirebaseOptions(
-          apiKey: 'AIzaSyAZ5EGt_vaWHkusW0WDgV7qDM9fYK4ggAg',
-          appId: '1:680072344368:web:ed5bc871e101c5ebbbcef5',
-          messagingSenderId: '680072344368',
-          projectId: 'insta-clone-16e81',
-          authDomain: 'insta-clone-16e81.firebaseapp.com',
-          storageBucket: 'insta-clone-16e81.appspot.com',
-        )
-      );
-    } else {
-      await Firebase.initializeApp(
+      apiKey: 'AIzaSyAZ5EGt_vaWHkusW0WDgV7qDM9fYK4ggAg',
+      appId: '1:680072344368:web:ed5bc871e101c5ebbbcef5',
+      messagingSenderId: '680072344368',
+      projectId: 'insta-clone-16e81',
+      authDomain: 'insta-clone-16e81.firebaseapp.com',
+      storageBucket: 'insta-clone-16e81.appspot.com',
+    ));
+  } else {
+    await Firebase.initializeApp(
         options: const FirebaseOptions(
-          apiKey: 'AIzaSyASgD8Qi40wZZ3i-oxuJ6Rs412bu_GBbs0',
-          appId: '1:680072344368:android:9f41a8d648208c77bbcef5',
-          messagingSenderId: '680072344368',
-          projectId: 'insta-clone-16e81',
-          storageBucket: 'insta-clone-16e81.appspot.com',
-        )
-      );
-    }
+      apiKey: 'AIzaSyASgD8Qi40wZZ3i-oxuJ6Rs412bu_GBbs0',
+      appId: '1:680072344368:android:9f41a8d648208c77bbcef5',
+      messagingSenderId: '680072344368',
+      projectId: 'insta-clone-16e81',
+      storageBucket: 'insta-clone-16e81.appspot.com',
+    ));
+  }
   runApp(const MyApp());
 }
 
@@ -43,38 +42,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Instagram Clone',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: mobileBackgroundColor
-      ),
-      home: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot){
-          if(snapshot.connectionState == ConnectionState.active){
-            if(snapshot.hasData){
-              return const ResponsiveLayout(
-                  webScreenLayout: WebScreenLayout(),
-                  mobileScreenLayout: MobileScreenLayout()
-              );
-            } else if (snapshot.hasError) {
-              return Center(
-                child: Text('${snapshot.error}'),
-              );
-            }
-          }
-          if(snapshot.connectionState == ConnectionState.waiting){
-            return const Center(
-              child: CircularProgressIndicator(
-                color: primaryColor
-              ),
-            );
-          }
-          return const LoginScreen();
-        },
-      )
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => UserProvider())],
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Instagram Clone',
+          theme: ThemeData.dark()
+              .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
+          home: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.active) {
+                if (snapshot.hasData) {
+                  return const ResponsiveLayout(
+                      webScreenLayout: WebScreenLayout(),
+                      mobileScreenLayout: MobileScreenLayout());
+                } else if (snapshot.hasError) {
+                  return Center(
+                    child: Text('${snapshot.error}'),
+                  );
+                }
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(color: primaryColor),
+                );
+              }
+              return const LoginScreen();
+            },
+          )),
     );
   }
-
 }
--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,12 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:insta_clone/providers/user_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:insta_clone/models/user.dart' as model;
 
-class MobileScreenLayout extends StatelessWidget {
+class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({super.key});
 
   @override
+  State<MobileScreenLayout> createState() => _MobileScreenLayoutState();
+}
+
+class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('This is mobile')),
+    model.User user = Provider.of<UserProvider>(context).getUser;
+    return Scaffold(
+      body: Center(
+        child: Text(user.email),
+      ),
     );
   }
 }
