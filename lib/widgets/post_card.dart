0b877db2523cb09653f25be96c02eed8850c3f52
PetScaramussi <peterson.silva6@fatec.sp.gov.br>
2023-07-06 13:27:49 -0300

    add deleting post method

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -96,7 +96,10 @@ class _PostCardState extends State<PostCard> {
                             ]
                                 .map(
                                   (e) => InkWell(
-                                    onTap: () {},
+                                    onTap: () async {
+                                      FirestoreMethods().deletePost(widget.snap['postId']);
+                                      Navigator.of(context).pop();
+                                    },
                                     child: Container(
                                       padding: const EdgeInsets.symmetric(
                                           vertical: 12, horizontal: 16),
